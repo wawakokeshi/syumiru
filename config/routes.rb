96ce9d94703f7b0e139cc356185c66f5fc4dc01c
wawakokeshi <97097377+wawakokeshi@users.noreply.github.com>
2022-06-19 12:24:03 +0000

    [Add]PublicとAdmin追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  devise_for :admins
   devise_for :members
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  devise_for :members, skip: [:passwords], controllers: {
+  registrations: "public/registrations",
+  sessions: 'public/sessions'
+}
+  
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+}
+
 end
